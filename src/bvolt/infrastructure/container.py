8f220e6729341c02f83a3e7a9763d101aab128da
vdella <dellatsantos@gmail.com>
2026-01-15 12:33:09 -0300

    wired telemetry and services with influx adapter

--- src/bvolt/infrastructure/container.py
+++ src/bvolt/infrastructure/container.py
@@ -1,24 +1,36 @@
+from bvolt.domain.battery.model import Battery
 from bvolt.services.telemetry_service import TelemetryService
 from bvolt.services.battery_service import BatteryService
 from bvolt.services.microgrid_service import MicrogridService
+from bvolt.telemetry.influx_adapter import InfluxTelemetryReader
 
 
-def build_telemetry_service(telemetry_reader, telemetry_writer) -> TelemetryService:
+def build_telemetry_service() -> TelemetryService:
     """
     Construct the TelemetryService with the appropriate reader/writer.
     """
-    return TelemetryService(telemetry_reader, telemetry_writer)
+    telemetry_reader = InfluxTelemetryReader()
+    return TelemetryService(telemetry_reader)
 
 
-def build_battery_services() -> list[BatteryService]:
-    """
-    Construct BatteryService instances for all configured batteries.
-    """
-    raise NotImplementedError
+def build_battery_services(battery_id: str) -> list[BatteryService]:
+    # TODO: load battery definitions from config
+    battery = Battery(asset_id=battery_id)
+    telemetry = build_telemetry_service()
+
+    return [
+        BatteryService(
+            battery=battery,
+            telemetry=telemetry,
+        )
+    ]
 
 
 def build_microgrid_service() -> MicrogridService:
-    """
-    Construct the MicrogridService from all asset services.
-    """
-    raise NotImplementedError
+    batteries = build_battery_services()
+    telemetry = build_telemetry_service()
+
+    return MicrogridService(
+        batteries=batteries,
+        telemetry=telemetry,
+    )
